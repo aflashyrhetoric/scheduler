ab1d736b57b62be2750b959f319b11a4f976ed8d
Kevin Oh <aflashyrhetoric@gmail.com>
2018-09-07 22:03:14 -0400

    Added some filtering and stuff to able to access student data

--- src/index.js
+++ src/index.js
@@ -1,90 +1,90 @@
-import studentRoster from './students';
-import timeslots from './timeslots';
+import studentRoster from './students'
+import timeslots from './timeslots'
 
-let studentList = studentRoster.students;
+let studentList = studentRoster.students
 
-let colleges = [];
-const HOUR = 60;
+let colleges = []
+const HOUR = 60
 
 class Time {
   constructor(hours, minutes) {
-    this.hours = hours;
-    this.minutes = minutes;
+    this.hours = hours
+    this.minutes = minutes
   }
   convertToSeconds() {
-    let seconds = 0;
-    seconds += this.hours * 60 /* minutes */ * 60 /* seconds */;
-    seconds += this.minutes * 60 /* seconds */;
-    return seconds;
+    let seconds = 0
+    seconds += this.hours * 60 /* minutes */ * 60 /* seconds */
+    seconds += this.minutes * 60 /* seconds */
+    return seconds
   }
 }
 
 function getEndTime(startTime, duration) {
-  let newTime = {};
-  let remainder = (startTime.minutes + duration) % 60;
+  let newTime = {}
+  let remainder = (startTime.minutes + duration) % 60
 
   if(startTime.minutes + duration >= 60) {
-    newTime = new Time(startTime.hours + 1, remainder);
+    newTime = new Time(startTime.hours + 1, remainder)
   } else {
-    newTime = new Time(startTime.hours, startTime.minutes + remainder);
+    newTime = new Time(startTime.hours, startTime.minutes + remainder)
   }
-  return newTime;
+  return newTime
 }
 
 function pp(time) {
-  let AMPM = time.hours < 12 ? 'AM' : 'PM';
-  let displayHours = time.hours > 12 ? time.hours % 12 : time.hours;
+  let AMPM = time.hours < 12 ? 'AM' : 'PM'
+  let displayHours = time.hours > 12 ? time.hours % 12 : time.hours
   if(time.minutes == 0) {
-    time.minutes = '00';
+    time.minutes = '00'
   }
-  return `${displayHours}:${time.minutes}${AMPM}`;
+  return `${displayHours}:${time.minutes}${AMPM}`
 }
 
 function flatten(arr1) {
-  return arr1.reduce((acc, val) => acc.concat(val), []);
+  return arr1.reduce((acc, val) => acc.concat(val), [])
 }
 
 
 class Course {
   constructor(name, startHour, startMinute, duration) {
-    this.name = name;
-    this.startTime = new Time(startHour, startMinute);
-    this.duration = duration;
-    this.endTime = getEndTime(this.startTime, this.duration);
+    this.name = name
+    this.startTime = new Time(startHour, startMinute)
+    this.duration = duration
+    this.endTime = getEndTime(this.startTime, this.duration)
   }
 
   fitsIn(timeSlot) {
-    let timeSlotStart = timeSlot.startTime.convertToSeconds();
-    let timeSlotEnd = timeSlot.endTime.convertToSeconds();
-    let startTimeInSeconds = this.startTime.convertToSeconds();
-    let endTimeInSeconds = this.endTime.convertToSeconds();
+    let timeSlotStart = timeSlot.startTime.convertToSeconds()
+    let timeSlotEnd = timeSlot.endTime.convertToSeconds()
+    let startTimeInSeconds = this.startTime.convertToSeconds()
+    let endTimeInSeconds = this.endTime.convertToSeconds()
 
     return timeSlotStart >= startTimeInSeconds &&
-           timeSlotEnd   <= endTimeInSeconds;
+           timeSlotEnd   <= endTimeInSeconds
   }
 }
 
 class Wheatley extends Course {
   constructor(startHour, startMinute) {
-    super('Wheatley', startHour, startMinute, 50);
+    super('Wheatley', startHour, startMinute, 50)
   }
 }
 
 class Reading extends Course {
   constructor(startHour, startMinute) {
-    super('Reading', startHour, startMinute, 60);
+    super('Reading', startHour, startMinute, 60)
   }
 }
 
 class Writing extends Course {
   constructor(startHour, startMinute) {
-    super('Writing', startHour, startMinute, 45);
+    super('Writing', startHour, startMinute, 45)
   }
 }
 
 class SharedText extends Course {
   constructor(startHour, startMinute) {
-    super('SharedText', startHour, startMinute, 25);
+    super('SharedText', startHour, startMinute, 25)
   }
 }
 
@@ -97,10 +97,10 @@ let Hampton = {
     new SharedText(14, 15)
   ],
 }
-let Pittsburgh = Object.assign({}, Hampton);
-Pittsburgh.name = 'Pittsburgh';
-let Temple = Object.assign({}, Hampton);
-Temple.name = 'Temple';
+let Pittsburgh = Object.assign({}, Hampton)
+Pittsburgh.name = 'Pittsburgh'
+let Temple = Object.assign({}, Hampton)
+Temple.name = 'Temple'
 let Maryland = {
   name: 'Maryland',
   courses: [
@@ -109,7 +109,7 @@ let Maryland = {
     new SharedText(12, 2, 30),
     new Wheatley(13, 15),
   ],
-};
+}
 let USC = {
   name: 'USC',
   courses: [
@@ -118,7 +118,7 @@ let USC = {
     new SharedText(9, 47, 30),
     new Reading(11, 0),
   ],
-};
+}
 let Albany = {
   name: 'Albany',
   courses: [
@@ -127,7 +127,7 @@ let Albany = {
     new Writing(1, 15, 50),
     new SharedText(15, 5, 30),
   ],
-};
+}
 
 // 6 schools with 4 courses each
 colleges.push(
@@ -137,52 +137,60 @@ colleges.push(
   Maryland,
   USC,
   Albany
-);
+)
 
 // Add kids to colleges
 colleges.forEach(college => {
-  college.members = [];
+  college.members = []
   studentList.forEach(student => {
     if(college.name == student.college) {
-      college.members.push(student);
+      college.members.push(student)
     }
   })
-});
+})
 
 /*
  * Get all courses 
  */
 
+// college - courses join
 let collegeCourseMap = colleges.map(college => {
   return {
     name: college.name,
     courses: college.courses
   }
-});
-collegeCourseMap = flatten(collegeCourseMap);
-
+})
+collegeCourseMap = flatten(collegeCourseMap)
 
 timeslots.forEach(slot => {
-  let allCurrentCourses = [];
+  let allCurrentCourses = []
 
   collegeCourseMap.forEach( college => {
-    let currentCollegeCourses = [];
-    currentCollegeCourses = college.courses.filter( college => college.fitsIn(slot));
+    let currentCollegeCourses = []
+    currentCollegeCourses = college.courses.filter( college => college.fitsIn(slot))
     if(currentCollegeCourses.length > 0) {
-      allCurrentCourses = allCurrentCourses.concat({ college: college.name, courses: currentCollegeCourses});
+      allCurrentCourses = allCurrentCourses.concat({ college: college.name, courses: currentCollegeCourses})
     }
-  });
+  })
 
-  // collegeCourseMap.forEach( collegeCourseMapping => {
-  //   currentCourses = collegeCourseMapping.filter( college => {
-  //     return college.fitsIn(slot);
-  //   });
-  // });
+  // printOverview(allCurrentCourses)
 
+  colleges.forEach(college => {
+    // List of kids in this college
+    let currentCollegeStudents = studentList
+        .filter(student => student.college == college.name)
+        // .map(student => student.name)
+
+    console.log(`${college.name} - ${currentCollegeStudents}`)
+  })
+})
+
+// Given a list of courses at a given time slot, prints it
+function printOverview(allCurrentCourses) {
   if(allCurrentCourses.length > 0) {
-    const courseName = allCurrentCourses.map(course => `${course.courses.map(course => course.name)} (${course.college})`);
-    console.log(`The courses that fit in the ${slot} are ${courseName}`);
+    const courseName = allCurrentCourses.map(course => `${course.courses.map(course => course.name)} (${course.college})`)
+    console.log(`The courses that fit in the ${slot} are ${courseName}`)
   } else {
-    console.log(`There are no courses at ${slot} that you can sit in on.`);
+    console.log(`There are no courses at ${slot} that you can sit in on.`)
   }
-});
+}
